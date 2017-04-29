7f0b90f49caa21135c8f6d34f41f8512e1ee17a5
Javier <great_jabibip@msn.com>
2017-04-29 17:31:18 +0300

    Final touches

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,31 @@
 $(document).ready(function(){
 
-	$("#projects-list a").mouseover(function(event){
+	$(".toggle").click(function() {
 
+		var $menu = $("#menu");
+
+		$("#emptydiv").append($menu);
+		$menu.attr("display", "flex");
 		
+	  	$(this).toggleClass("on");
+	  	$menu.fadeToggle();
 	});
 
+/* This section is to get the animated scroll effect */
+
+	var $menubutton = $("#main-content a");
+
+	$($menubutton).on('click',function (e) {
+	    
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
 });
